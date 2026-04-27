3ca49f45a462d13f1cdd23b2fa50b78ddaaab60a
畠中 太朗 <k024g0045@g.neec.ac.jp>
2026-04-27 11:31:50 +0900

    summaryコメントの追加

--- include/engine/animation/AssimpAnimationLoader.h
+++ include/engine/animation/AssimpAnimationLoader.h
@@ -7,5 +7,10 @@
 /// </summary>
 class AssimpAnimationLoader {
   public:
+    /// <summary>
+    /// シーンからアニメーション情報をモデルへ書き込む
+    /// </summary>
+    /// <param name="scene">Assimpのシーンデータ</param>
+    /// <param name="model">書き込み先モデル</param>
     void LoadAnimations(const aiScene *scene, Model &model) const;
 };
